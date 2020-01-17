9393e017feeeb14f0d56ca3ee31b45000cb7a56e
lucian-vasile <lucian.vasile@live.com>
2020-01-17 19:36:05 +0200

    Update media.bat

--- media.bat
+++ media.bat
@@ -9,6 +9,3 @@ winamp ^
 
 curl -o %TEMP%/spotify.exe -L https://www.spotify.com/ro/download/
 start %TEMP%/spotify.exe
-
-curl -o %TEMP%/simplednla.exe -L https://github.com/nmaier/simpleDLNA/releases/download/v1.0/simpledlna-1.0.msi
-start %TEMP%/simplednla.exe
